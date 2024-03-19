d547f03d92ced1ab5bcd30ca40f38d7926267e6b
razanemeguellati <kl_meguellati@esi.dz>
2024-03-19 23:26:39 +0100

    eloquent models

--- app/Http/Controllers/DrinksController.php
+++ app/Http/Controllers/DrinksController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Drink;
 
 class DrinksController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $drinks = Drink::all();
+        return view('drinks' , ['drinks'=> $drinks]);
     }
     public function show($id){
         return view('details' , ['id'=> $id]);
--- resources/views/drinks.blade.php
+++ resources/views/drinks.blade.php
@@ -3,10 +3,15 @@
 
 <div class="flex-center position-ref full-height">      
         <div class="content">
-        <img src="/img/background.png" alt="">
+           <img src="/img/background.png" alt="">
             <div class="title m-b-md">
                 Esi's Best Drinks
             </div>
+
+            @foreach($drinks as $drink)
+            <p> {{ $drink['name'] }}</p>
+            @endforeach
+
         </div>
  
 </div>
