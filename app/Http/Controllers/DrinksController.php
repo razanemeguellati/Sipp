4655a30e199ca83eaf2ac3b74a05c03b05df41df
razanemeguellati <kl_meguellati@esi.dz>
2024-03-20 00:00:00 +0100

    eloquent models p3

--- app/Http/Controllers/DrinksController.php
+++ app/Http/Controllers/DrinksController.php
@@ -9,16 +9,21 @@ class DrinksController extends Controller
 {
     public function index(){
         $drinks = Drink::all();
-        
+
         //argument is the column name but unlike all() we have to add the get method to have the data 
         //$drinks= Drink::orderby('id','desc')->get();
 
         //1st arg is the column an 2nd is its value 
         //$drinks = Drink::where('type','kabyle')->get();
 
-        return view('drinks' , ['drinks'=> $drinks]);
+        return view('indexDrinks' , ['drinks'=> $drinks]);
     }
+
     public function show($id){
         return view('details' , ['id'=> $id]);
     }  
+
+    public function create(){
+        return view('details' );
+    } 
 }
